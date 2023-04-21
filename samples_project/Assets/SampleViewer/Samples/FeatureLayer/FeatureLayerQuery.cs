572868dab036f82feb2acb58474b9e60099ab2fa
Jade Huang <jad10309@esri.com>
2023-04-20 17:21:43 -0700

    improve raycasting method, visualize trees according to parameters

--- samples_project/Assets/SampleViewer/Samples/FeatureLayer/FeatureLayerQuery.cs
+++ samples_project/Assets/SampleViewer/Samples/FeatureLayer/FeatureLayerQuery.cs
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +16,7 @@ using Esri.GameEngine.Geometry;
 // in order to leverage Unity's JsonUtility.
 // When implementing your own version of this the Baseball Properties would need to 
 // be updated.
+
 [System.Serializable]
 public class FeatureCollectionData
 {
@@ -29,6 +30,7 @@ public class Feature
     public string type;
     public Geometry geometry;
     public TreeProperties properties;
+    
 }
 
 [System.Serializable]
@@ -50,7 +52,7 @@ public class Geometry
 // with correct property values. This is a good starting point if you are looking to parse your own feature layer into Unity.
 public class FeatureLayerQuery : MonoBehaviour
 {
-    // The feature layer we are going to query
+    // The preview of this service is here https://runtime.maps.arcgis.com/home/item.html?id=05c3f9d7dea6422b86e30967811bddd7
     public string FeatureLayerURL = "https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Baumkataster_Berlin/FeatureServer/0";
 
     // This prefab will be instatiated for each feature we parse
@@ -66,19 +68,27 @@ public class FeatureLayerQuery : MonoBehaviour
     private int FeatureSRWKID = 4326;
 
     public ArcGISCameraComponent ArcGISCamera;
-
+    public Dropdown TreeSelector;
 
     // Get all the features when the script starts
     void Start()
     {
         StartCoroutine(GetFeatures());
-/*
-        StadiumSelector.onValueChanged.AddListener(delegate
-        {
-            StadiumSelected();
-        });*/
-    }
 
+        TreeSelector.onValueChanged.AddListener(delegate
+        {
+            TreeSelected();
+        }); 
+    }
+    private Dictionary<string, string> genusToPrefabPath = new Dictionary<string, string>
+    {
+        { "ACER", "Assets/XFrog/2022_PBR_XfrogPlants_Sampler/Prefabs/MESH_EU01_AcerCampestre_A_LOD0.fbx" },
+        { "MELALEUCA", "Assets/XFrog/2022_PBR_XfrogPlants_Sampler/Prefabs/MESH_OC56_MelaleucaAlternifolia_Y_LOD0" },
+        { "DRACAENA,", "Assets/XFrog/2022_PBR_XfrogPlants_Sampler/Prefabs/MESH_AF08_DracaenaDraco_A_LOD0.fbx" },
+        { "NERIUM", "Assets/XFrog/2022_PBR_XfrogPlants_Sampler/Prefabs/MESH_BS09_NeriumOleander_A_LOD0.fbx" },
+        { "CEDRUS", "Assets/XFrog/2022_PBR_XfrogPlants_Sampler/Prefabs/MESH_CL04_CalocedrusDecurrens_A_LOD0.fbx.fbx" },
+        
+    };
     // Sends the Request to get features from the service
     private IEnumerator GetFeatures()
     {
@@ -95,8 +105,9 @@ public class FeatureLayerQuery : MonoBehaviour
         }
         else
         {
+
             CreateGameObjectsFromResponse(Request.downloadHandler.text);
-        //    PopulateStadiumDropdown();
+            PopulateTreeDropdown();
         }
     }
 
@@ -105,7 +116,8 @@ public class FeatureLayerQuery : MonoBehaviour
     // where=1=1 gets every feature. geometry based or more intelligent where clauses should be used
     //     with larger datasets
     // outSR=4326 gets the return geometries in the SR 4326
-    // outFields=LEAGUE,TEAM,NAME specifies the fields we want in the response
+    // outFields specifies the fields we want in the response
+    // only getting the first 500 of the trees for the performance purpose
     private string MakeRequestHeaders()
     {
         string[] OutFields =
@@ -128,15 +140,33 @@ public class FeatureLayerQuery : MonoBehaviour
                 OutFieldHeader += ",";
             }
         }
-
+       // string district = "Mitte";
+        /*
         string[] RequestHeaders =
         {
             "f=geojson",
-            "where=1=1",
+            $"where=bezirk%3D%27{district}%27",
             "outSR=" + FeatureSRWKID.ToString(),
-            OutFieldHeader
+            OutFieldHeader,
+            "resultRecordCount=1500"
         };
-
+        */
+        double lat = 52.5145;
+        double lon = 13.3501;
+        int radius = 1000;
+        string[] RequestHeaders =
+    {
+        "f=geojson",
+        "geometryType=esriGeometryPoint",
+        $"geometry={lon},{lat}",
+        "spatialRel=esriSpatialRelIntersects",
+        $"distance={radius}",
+        "units=esriSRUnit_Meter",
+        "inSR=" + FeatureSRWKID.ToString(),
+        "outSR=" + FeatureSRWKID.ToString(),
+        OutFieldHeader,
+       
+    };
         string ReturnValue = "";
         for (int i = 0; i < RequestHeaders.Length; i++)
         {
@@ -158,16 +188,37 @@ public class FeatureLayerQuery : MonoBehaviour
     {
         // Deserialize the JSON response from the query.
         var deserialized = JsonUtility.FromJson<FeatureCollectionData>(Response);
-
+        int counter = 0;
         foreach (Feature feature in deserialized.features)
         {
             double Longitude = feature.geometry.coordinates[0];
             double Latitude = feature.geometry.coordinates[1];
+            string genus = feature.properties.genus;
+            double crownDiameter = Convert.ToDouble(feature.properties.crown);
+            double height = Convert.ToDouble(feature.properties.height);
 
             ArcGISPoint Position = new ArcGISPoint(Longitude, Latitude, TreeSpawnHeight, new ArcGISSpatialReference(FeatureSRWKID));
+            float scaleHeight = (float)(height / TreePrefab.transform.localScale.y);
+            float scaleCrown = (float)(crownDiameter / TreePrefab.transform.localScale.x);
 
-            var NewTree = Instantiate(TreePrefab, this.transform);
-            //   NewStadium.name = feature.properties.NAME;
+
+            GameObject NewTree;
+            string prefabPath;
+            if (genus!=null && genusToPrefabPath.TryGetValue(genus, out prefabPath))
+            {
+               NewTree = Instantiate((GameObject)Resources.Load(prefabPath), this.transform);
+                Debug.LogWarning($"found trees");
+
+            }
+       /*     else
+			{
+                NewTree = Instantiate(TreePrefab, this.transform);
+             //   Debug.LogWarning($"Genus '{genus}' not found in the dictionary. Skipping this tree.");
+            }*/
+
+            NewTree.transform.localScale = new Vector3(scaleCrown, scaleHeight, scaleCrown);
+            NewTree.name = feature.properties.genus + "_" + counter.ToString();
+            counter++;
             Trees.Add(NewTree);
             NewTree.SetActive(true);
 
@@ -183,6 +234,45 @@ public class FeatureLayerQuery : MonoBehaviour
 
             TreeInfo.ArcGISCamera = ArcGISCamera;
             TreeInfo.SetSpawnHeight(TreeSpawnHeight);
+        }
+
+    }
+
+
+    private void PopulateTreeDropdown()
+    {
+
+        List<string> TreeNames = new List<string>();
+        foreach (GameObject tree in Trees)
+        {
+            TreeNames.Add(tree.name);
+        }
+        TreeNames.Sort();
+        TreeSelector.AddOptions(TreeNames);
+    }
+
+
+    private void TreeSelected()
+    {
+        var TreeName = TreeSelector.options[TreeSelector.value].text;
+        foreach (GameObject tree in Trees)
+        {
+            if (TreeName == tree.name)
+            {
+                var TreeLocation = tree.GetComponent<ArcGISLocationComponent>();
+                if (TreeLocation == null)
+                {
+                    return;
+                }
+                var CameraLocation = ArcGISCamera.GetComponent<ArcGISLocationComponent>();
+                double Longitude = TreeLocation.Position.X;
+                double Latitude = TreeLocation.Position.Y;
+                double CameraAltitude = TreeLocation.Position.Z + 20;
+                ArcGISPoint NewPosition = new ArcGISPoint(Longitude, Latitude, CameraAltitude, TreeLocation.Position.SpatialReference);
+
+                CameraLocation.Position = NewPosition;
+                CameraLocation.Rotation = TreeLocation.Rotation;
+            }
         }
     }
 }
--- samples_project/Assets/SampleViewer/Samples/FeatureLayer/Tree.cs
+++ samples_project/Assets/SampleViewer/Samples/FeatureLayer/Tree.cs
@@ -3,12 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Esri.ArcGISMapsSDK.Components;
 using Esri.GameEngine.Geometry;
 using Esri.HPFramework;
+
 
 
 // This class holds information for each stadium, controls how they are rendered, and
@@ -24,8 +25,7 @@ public class Tree : MonoBehaviour
     private double SpawnHeight = 10000;
     public double RayCastDistanceThreshold = 300000;
     private bool OnGround = false;
-    private int Counter = 0;
-    public int UpdatesPerRayCast = 200;
+
 
     public void SetInfo(string Info)
     {
@@ -33,9 +33,6 @@ public class Tree : MonoBehaviour
 
         // Based on which leage team belongs to, either the national or american league, we will render the stadium differently
         // See StadiumMaterial.shadergraph for how this is being accomplished
-
-       
-
     }
 
     // Used to tell this object how high it was spawned so we can control the distance of the raycast
@@ -46,24 +43,9 @@ public class Tree : MonoBehaviour
 
     public void Start()
     {
-        // Starting the counter at a random value makes it so we won't do raycast calculation on the same tick
-        Counter = Random.Range(0, UpdatesPerRayCast);
-    }
+        // Your existing code
 
-    public void Update()
-    {
-        // Check each object every UpdatesPerRayCast updates to see if it was placed on the ground yet
-        if (OnGround)
-        {
-            return;
-        }
-
-        Counter++;
-        if (Counter >= UpdatesPerRayCast)
-        {
-            Counter = 0;
-            SetOnGround();
-        }
+        StartCoroutine(SetOnGround());
     }
 
     // This Feature Layer does not contain information about the feature's altitude.
@@ -73,28 +55,43 @@ public class Tree : MonoBehaviour
     // at so the hit test wouldn't work for objects that don't have loaded terrain underneath them
     // Another way to get the elevation would be to query/identify the elevation service you are using for each
     // feature to discover the altitude
-    private void SetOnGround()
+
+   
+    private IEnumerator SetOnGround()
     {
-        var CameraHP = ArcGISCamera.GetComponent<HPTransform>();
-        var HP = transform.GetComponent<HPTransform>();
-        var Distance = (CameraHP.UniversePosition - HP.UniversePosition).ToVector3().magnitude;
+        //updating position multiple times to increase positioning accuracy
+        //you can modify this number to place the object more accurately 
+        int maxAttempts = 30; // maximum number of attempts
+        int currentAttempt = 0;
 
-        if (Distance < RayCastDistanceThreshold)
+        while (!OnGround && currentAttempt < maxAttempts)
         {
-            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, (float)SpawnHeight))
+            currentAttempt++;
+
+            var CameraHP = ArcGISCamera.GetComponent<HPTransform>();
+            var HP = transform.GetComponent<HPTransform>();
+            var Distance = (CameraHP.UniversePosition - HP.UniversePosition).ToVector3().magnitude;
+
+            if (Distance < RayCastDistanceThreshold)
             {
-                // Modify the Stadiums altitude based off the raycast hit
-                var TreeLocationComponent = transform.GetComponent<ArcGISLocationComponent>();
-                double NewHeight = TreeLocationComponent.Position.Z - hitInfo.distance;
-                double TreeLongitude = TreeLocationComponent.Position.X;
-                double TreeLatitude = TreeLocationComponent.Position.Y;
-                ArcGISPoint Position = new ArcGISPoint(TreeLongitude, TreeLatitude, NewHeight, TreeLocationComponent.Position.SpatialReference);
-                TreeLocationComponent.Position = Position;
+                if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, (float)SpawnHeight))
+                {
+                    // Modify the Stadiums altitude based off the raycast hit
+                    var TreeLocationComponent = transform.GetComponent<ArcGISLocationComponent>();
+                    double NewHeight = TreeLocationComponent.Position.Z - hitInfo.distance;
+                    double TreeLongitude = TreeLocationComponent.Position.X;
+                    double TreeLatitude = TreeLocationComponent.Position.Y;
+                    ArcGISPoint Position = new ArcGISPoint(TreeLongitude, TreeLatitude, NewHeight+0.5, TreeLocationComponent.Position.SpatialReference);
+                    TreeLocationComponent.Position = Position;
 
-                OnGround = true;
-
+                    OnGround = true;
+                }
             }
+            // Wait for a short period before trying again
+            yield return new WaitForSeconds(0.1f);
         }
     }
+
+    
 
 }
